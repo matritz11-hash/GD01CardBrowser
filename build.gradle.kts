4caca0a4bd2a40be1735697d5a18fa388416dc36
matritz11-hash <mat.ritz11@gmail.com>
2025-08-14 14:16:03 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
